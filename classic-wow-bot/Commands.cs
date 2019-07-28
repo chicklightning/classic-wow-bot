379bf821dfb490b04e02bca8f11f7b59b1274231
Gabrielle <chicklightning@users.noreply.github.com>
2019-07-28 16:56:53 -0700

    Add project files.

---
+++ classic-wow-bot/Commands.cs
@@ -0,0 +1,283 @@
+﻿using AngleSharp;
+using AngleSharp.Html.Parser;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+namespace ClassicWoWBot
+{
+    public class ServerCommands
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        [Command("status")] // let's define this method as a command
+        [Description("Shows the current status of the Light's Hope classic server.")] // this will be displayed to tell users what this command does when they invoke help
+        public async Task Status(CommandContext context) // this command takes no arguments
+        {
+            // let's trigger a typing indicator to let users know we're working
+            await context.TriggerTypingAsync();
+
+            // Use the default configuration for AngleSharp
+            var config = Configuration.Default;
+
+            // Create a new context for evaluating webpages with the given config
+            var browseContext = BrowsingContext.New(config);
+
+            // Source to be parsed
+            string response = await client.GetStringAsync("https://status.lightshope.org/");
+
+            // Create a virtual request to specify the document to load (here from our fixed string)
+            var document = await browseContext.OpenAsync(req => req.Content(response));
+
+            // Grab the first relevant section, <div class="alert ..."></div> from the doc
+            var generalStatusDiv = document.QuerySelector("div.alert");
+            string generalStatus = generalStatusDiv.TextContent;
+
+            // let's make the message a bit more colourful
+            DiscordEmoji generalStatusEmoji;
+            if (generalStatus.Contains("operational"))
+            {
+                // Set to 👌
+                generalStatusEmoji = DiscordEmoji.FromName(context.Client, ":ok_hand:");
+            }
+            else
+            {
+                // Set to 👎
+                generalStatusEmoji = DiscordEmoji.FromName(context.Client, ":thumbsdown:");
+            }
+
+            // Grab all <small class="text-component-1 ..."></small> from the doc
+            var allSmall = document.QuerySelectorAll("small.text-component-1");
+
+            // the sixth element in this list is the Northdale server
+            string serverStatus = $"Northdale Server is {allSmall[5].TextContent.ToLower()}";
+
+            DiscordEmoji serverStatusEmoji;
+            DiscordColor textColor;
+            if (generalStatus.Contains("operational"))
+            {
+                // Set to 👌
+                serverStatusEmoji = DiscordEmoji.FromName(context.Client, ":ok_hand:");
+
+                textColor = DiscordColor.PhthaloGreen;
+            }
+            else
+            {
+                // Set to 👎
+                serverStatusEmoji = DiscordEmoji.FromName(context.Client, ":thumbsdown:");
+
+                textColor = DiscordColor.Red;
+            }
+
+            // wrap it into an embed
+            var embed = new DiscordEmbedBuilder
+            {
+                Title = "Light's Hope Status",
+                Description = $"{serverStatusEmoji} {serverStatus} | {generalStatusEmoji} {generalStatus}",
+
+                Timestamp = DateTime.Now
+            };
+
+            // text color green if operational, otherwise red
+            embed.WithColor(textColor);
+
+            // respond with content
+            await context.RespondAsync(embed: embed);
+        }
+
+        [Command("incidents")]
+        [Description("Says whether any incidents have occurred today in Light's Hope servers.")]
+        public async Task Incidents(CommandContext context)
+        {
+            // let's trigger a typing indicator to let
+            // users know we're working
+            await context.TriggerTypingAsync();
+
+            // Use the default configuration for AngleSharp
+            var config = Configuration.Default;
+
+            // Create a new context for evaluating webpages with the given config
+            var browseContext = BrowsingContext.New(config);
+
+            // Source to be parsed
+            string response = await client.GetStringAsync("https://status.lightshope.org/");
+
+            // Create a virtual request to specify the document to load (here from our fixed string)
+            var document = await browseContext.OpenAsync(req => req.Content(response));
+
+            // wrap it into an embed
+            var embed = new DiscordEmbedBuilder
+            {
+                Title = "Today's Incidents on Light's Hope"
+            };
+
+            // Grab the first relevant section, <div class="alert ..."></div> from the doc
+            var incidentDiv = document.QuerySelector("div.panel-body > p");
+            string incidentList = incidentDiv.TextContent;
+
+            DiscordEmoji incidentStatusEmoji;
+            DiscordColor textColor;
+            string incidentStatus;
+            if (incidentList.Equals("No incidents reported"))
+            {
+                // Set to 👌
+                incidentStatusEmoji = DiscordEmoji.FromName(context.Client, ":ok_hand:");
+                incidentStatus = "No incidents reported today.";
+            }
+            else
+            {
+                // Set to ❗
+                incidentStatusEmoji = DiscordEmoji.FromName(context.Client, ":exclamation:");
+                incidentStatus = "There was an incident today.";
+                textColor = DiscordColor.Gold;
+                embed.WithColor(textColor);
+                embed.WithUrl("https://status.lightshope.org/");
+            }
+
+            embed.WithDescription($"{incidentStatusEmoji} {incidentStatus}");
+
+            // send response
+            await context.RespondAsync(embed: embed);
+        }
+    }
+
+    public class WowHeadCommands
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        [Command("linkerator")] // let's define this method as a command
+        [Description("Shows the current status of the Light's Hope classic server.")] // this will be displayed to tell users what this command does when they invoke help
+        public async Task Linkerator(CommandContext context) // this command takes no arguments
+        {
+            // let's trigger a typing indicator to let users know we're working
+            await context.TriggerTypingAsync();
+
+            // Use the default configuration for AngleSharp
+            var config = Configuration.Default;
+
+            // Create a new context for evaluating webpages with the given config
+            var browseContext = BrowsingContext.New(config);
+
+            // Source to be parsed
+            string response = await client.GetStringAsync("https://status.lightshope.org/");
+
+            // Create a virtual request to specify the document to load (here from our fixed string)
+            var document = await browseContext.OpenAsync(req => req.Content(response));
+
+            // Grab the first relevant section, <div class="alert ..."></div> from the doc
+            var generalStatusDiv = document.QuerySelector("div.alert");
+            string generalStatus = generalStatusDiv.TextContent;
+
+            // let's make the message a bit more colourful
+            DiscordEmoji generalStatusEmoji;
+            if (generalStatus.Contains("operational"))
+            {
+                // Set to 👌
+                generalStatusEmoji = DiscordEmoji.FromName(context.Client, ":ok_hand:");
+            }
+            else
+            {
+                // Set to 👎
+                generalStatusEmoji = DiscordEmoji.FromName(context.Client, ":thumbsdown:");
+            }
+
+            // Grab all <small class="text-component-1 ..."></small> from the doc
+            var allSmall = document.QuerySelectorAll("small.text-component-1");
+
+            // the sixth element in this list is the Northdale server
+            string serverStatus = $"Northdale Server is {allSmall[5].TextContent.ToLower()}";
+
+            DiscordEmoji serverStatusEmoji;
+            DiscordColor textColor;
+            if (generalStatus.Contains("operational"))
+            {
+                // Set to 👌
+                serverStatusEmoji = DiscordEmoji.FromName(context.Client, ":ok_hand:");
+
+                textColor = DiscordColor.PhthaloGreen;
+            }
+            else
+            {
+                // Set to 👎
+                serverStatusEmoji = DiscordEmoji.FromName(context.Client, ":thumbsdown:");
+
+                textColor = DiscordColor.Red;
+            }
+
+            // wrap it into an embed
+            var embed = new DiscordEmbedBuilder
+            {
+                Title = "Light's Hope Status",
+                Description = $"{serverStatusEmoji} {serverStatus} | {generalStatusEmoji} {generalStatus}",
+
+                Timestamp = DateTime.Now
+            };
+
+            // text color green if operational, otherwise red
+            embed.WithColor(textColor);
+
+            // respond with content
+            await context.RespondAsync(embed: embed);
+        }
+
+        [Command("find")]
+        [Description("Says whether any incidents have occurred today in Light's Hope servers.")]
+        public async Task Find(CommandContext context)
+        {
+            // let's trigger a typing indicator to let
+            // users know we're working
+            await context.TriggerTypingAsync();
+
+            // Use the default configuration for AngleSharp
+            var config = Configuration.Default;
+
+            // Create a new context for evaluating webpages with the given config
+            var browseContext = BrowsingContext.New(config);
+
+            // Source to be parsed
+            string response = await client.GetStringAsync("https://status.lightshope.org/");
+
+            // Create a virtual request to specify the document to load (here from our fixed string)
+            var document = await browseContext.OpenAsync(req => req.Content(response));
+
+            // wrap it into an embed
+            var embed = new DiscordEmbedBuilder
+            {
+                Title = "Today's Incidents on Light's Hope"
+            };
+
+            // Grab the first relevant section, <div class="alert ..."></div> from the doc
+            var incidentDiv = document.QuerySelector("div.panel-body > p");
+            string incidentList = incidentDiv.TextContent;
+
+            DiscordEmoji incidentStatusEmoji;
+            DiscordColor textColor;
+            string incidentStatus;
+            if (incidentList.Equals("No incidents reported"))
+            {
+                // Set to 👌
+                incidentStatusEmoji = DiscordEmoji.FromName(context.Client, ":ok_hand:");
+                incidentStatus = "No incidents reported today.";
+            }
+            else
+            {
+                // Set to ❗
+                incidentStatusEmoji = DiscordEmoji.FromName(context.Client, ":exclamation:");
+                incidentStatus = "There was an incident today.";
+                textColor = DiscordColor.Gold;
+                embed.WithColor(textColor);
+                embed.WithUrl("https://status.lightshope.org/");
+            }
+
+            embed.WithDescription($"{incidentStatusEmoji} {incidentStatus}");
+
+            // send response
+            await context.RespondAsync(embed: embed);
+        }
+    }
+}
---
+++ classic-wow-bot/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Exceptions;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+
+namespace ClassicWoWBot
+{
+    public class Program
+    {
+        public DiscordClient Client { get; set; }
+        public CommandsNextModule Commands { get; set; }
+
+        public static void Main(string[] args)
+        {
+            // since we cannot make the entry method asynchronous,
+            // let's pass the execution to asynchronous code
+            var prog = new Program();
+            prog.RunBotAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task RunBotAsync()
+        {
+            var config = new DiscordConfiguration
+            {
+                Token = Environment.GetEnvironmentVariable("DISCORD_TOKEN"),
+                TokenType = TokenType.Bot,
+
+                AutoReconnect = true,
+                LogLevel = LogLevel.Debug,
+                UseInternalLogHandler = true
+            };
+
+            // then we want to instantiate our client
+            this.Client = new DiscordClient(config);
+
+            // next, let's hook some events, so we know
+            // what's going on
+            this.Client.Ready += this.Client_Ready;
+            this.Client.GuildAvailable += this.Client_GuildAvailable;
+            this.Client.ClientErrored += this.Client_ClientError;
+
+            // up next, let's set up our commands
+            var commandConfig = new CommandsNextConfiguration
+            {
+                // let's use the string prefix defined in config.json
+                StringPrefix = Environment.GetEnvironmentVariable("COMMAND_PREFIX"),
+
+                // enable mentioning the bot as a command prefix
+                EnableMentionPrefix = true
+            };
+
+            // and hook them up
+            this.Commands = this.Client.UseCommandsNext(commandConfig);
+
+            // let's hook some command events, so we know what's going on
+            this.Commands.CommandExecuted += this.Commands_CommandExecuted;
+            this.Commands.CommandErrored += this.Commands_CommandErrored;
+
+            // up next, let's register our commands
+            this.Commands.RegisterCommands<ServerCommands>();
+            this.Commands.RegisterCommands<WowHeadCommands>();
+
+            // set up our custom help formatter
+            this.Commands.SetHelpFormatter<SimpleHelpFormatter>();
+
+            // finally, let's connect and log in
+            await this.Client.ConnectAsync();
+
+            // when the bot is running, try doing <prefix>help
+            // to see the list of registered commands, and 
+            // <prefix>help <command> to see help about specific
+            // command.
+
+            // and this is to prevent premature quitting
+            await Task.Delay(-1);
+        }
+
+        private Task Client_Ready(ReadyEventArgs e)
+        {
+            // let's log the fact that this event occured
+            e.Client.DebugLogger.LogMessage(LogLevel.Info, "Classic WoW Bot", "Ready to process events.", DateTime.Now);
+
+            // since this method is not async, let's return
+            // a completed task, so that no additional work
+            // is done
+            return Task.CompletedTask;
+        }
+
+        private Task Client_GuildAvailable(GuildCreateEventArgs e)
+        {
+            // let's log the name of the guild that was just
+            // sent to our client
+            e.Client.DebugLogger.LogMessage(LogLevel.Info, "Classic WoW Bot", $"Guild available: {e.Guild.Name}", DateTime.Now);
+
+            // since this method is not async, let's return
+            // a completed task, so that no additional work
+            // is done
+            return Task.CompletedTask;
+        }
+
+        private Task Client_ClientError(ClientErrorEventArgs e)
+        {
+            // let's log the details of the error that just 
+            // occured in our client
+            e.Client.DebugLogger.LogMessage(LogLevel.Error, "Classic WoW Bot", $"Exception occured: {e.Exception.GetType()}: {e.Exception.Message}", DateTime.Now);
+
+            // since this method is not async, let's return
+            // a completed task, so that no additional work
+            // is done
+            return Task.CompletedTask;
+        }
+
+        private Task Commands_CommandExecuted(CommandExecutionEventArgs e)
+        {
+            // let's log the name of the command and user
+            e.Context.Client.DebugLogger.LogMessage(LogLevel.Info, "Classic WoW Bot", $"{e.Context.User.Username} successfully executed '{e.Command.QualifiedName}'", DateTime.Now);
+
+            // since this method is not async, let's return
+            // a completed task, so that no additional work
+            // is done
+            return Task.CompletedTask;
+        }
+
+        private async Task Commands_CommandErrored(CommandErrorEventArgs e)
+        {
+            // let's log the error details
+            e.Context.Client.DebugLogger.LogMessage(LogLevel.Error, "Classic WoW Bot", $"{e.Context.User.Username} tried executing '{e.Command?.QualifiedName ?? "<unknown command>"}' but it errored: {e.Exception.GetType()}: {e.Exception.Message ?? "<no message>"}", DateTime.Now);
+
+            // let's check if the error is a result of lack
+            // of required permissions
+            if (e.Exception is ChecksFailedException ex)
+            {
+                // yes, the user lacks required permissions, 
+                // let them know
+
+                var emoji = DiscordEmoji.FromName(e.Context.Client, ":no_entry:");
+
+                // let's wrap the response into an embed
+                var embed = new DiscordEmbedBuilder
+                {
+                    Title = "Access denied",
+                    Description = $"{emoji} You do not have the permissions required to execute this command.",
+                    Color = new DiscordColor(0xFF0000) // red
+                    // there are also some pre-defined colors available
+                    // as static members of the DiscordColor struct
+                };
+                await e.Context.RespondAsync("", embed: embed);
+            }
+        }
+    }
+}
